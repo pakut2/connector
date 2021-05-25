b0fed935c38f7fe6ebf1b72cd333aab84104060f
pakut2 <kutny.pawel@gmail.com>
2021-05-26 01:13:37 +0200

    routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-//connect DB
+// connect DB
 connectDB();
 
 const port = process.env.PORT || 5000;
@@ -11,6 +11,12 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("api running");
 });
+
+// define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
