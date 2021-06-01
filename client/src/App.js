c58bf999a1b662e0865757b219941b51918b8783
pakut2 <kutny.pawel@gmail.com>
2021-06-01 23:35:51 +0200

    create profile component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Alert from "./components/layout/Alert";
 import setAuthToken from "./utils/setAuthToken";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from "./components/profile-form/CreateProfile";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -36,6 +37,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
