6d7f07113204d1695c248d3e0b2a36ef5da5a700
pakut2 <kutny.pawel@gmail.com>
2021-06-03 02:37:14 +0200

    display all profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -31,7 +31,7 @@ export const getCurrentProfile = () => async (dispatch) => {
 };
 
 // Get all profiles
-export const getAllProfiles = () => async (dispatch) => {
+export const getProfiles = () => async (dispatch) => {
   dispatch({
     type: CLEAR_PROFILE,
   });
@@ -256,7 +256,7 @@ export const deleteEducation = (id) => async (dispatch) => {
 export const deleteAccount = () => async (dispatch) => {
   if (window.confirm("Are you sure? This can NOT be undone")) {
     try {
-      const res = await axios.delete(`/api/profile`);
+      await axios.delete(`/api/profile`);
 
       dispatch({
         type: CLEAR_PROFILE,
